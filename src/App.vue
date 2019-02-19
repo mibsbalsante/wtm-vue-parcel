6b71af5fa77f6408881c1089056a1cc1dcdcb937
Mibs Balsante <mibsbalsante@ckl.io>
2019-02-18 21:05:21 -0300

    feat: add project initial files

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<template>
+  <input type='text' v-model='field'>
+</template>
+
+<script>
+  import Vue from 'vue'
+
+  module.exports = {
+    name: 'app',
+    data() {
+      return { field: 'initial value' }
+    }
+  }
+</script>
