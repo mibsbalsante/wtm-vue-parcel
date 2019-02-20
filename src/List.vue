0eb0debbb529d9a8a02f5b709f6a95ad0c5c2b27
Mibs Balsante <mibsbalsante@ckl.io>
2019-02-19 22:18:08 -0300

    feat: show generated css code

--- src/List.vue
+++ src/List.vue
@@ -1,9 +1,16 @@
 <template>
   <ul class="list" v-if="list.length > 0">
     <li class="item" v-for="item in list" :key="item.name">
+      <h2 class="title">{{item.name}}</h2>
       <div
         :style="{width: `${item.width}px`, height: `${item.height}px`, backgroundColor: item.bgColor, border: `${item.borderWidth}px ${item.borderStyle} ${item.borderColor}`}"
-      >{{item.name}}</div>
+      ></div>
+      <pre class="pre">{
+        <code class="code">      width: {{item.width}}px,
+        height: {{item.height}}px,
+        backgroundColor: item.bgColor,
+        border: {{item.borderWidth}}px {{item.borderStyle}} {{item.borderColor}}</code>
+      }</pre>
     </li>
   </ul>
   <p class="empty" v-else>Você ainda não gerou retângulos :(</p>
@@ -28,8 +35,27 @@
     padding: 0;
   }
 
-  .list .item {
+  .item {
     padding: 15px 0;
+    display: flex;
+    justify-content: space-between;
+    flex-wrap: wrap;
+  }
+
+  .title {
+    width: 100%;
+  }
+
+  .pre {
+    white-space: pre-line;
+    margin: 0;
+    background-color: #eee;
+    padding: 5px;
+    height: 100%;
+  }
+
+  .code {
+    white-space: pre-wrap;
   }
 </style>
 
